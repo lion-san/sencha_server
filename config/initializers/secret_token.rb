d9a0ea166d36b8c58c486ff2cf37c6e5a66b012c
yokoi.shinya <shinya.yokoi@gmail.com>
2015-02-12 17:47:02 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SenchaServer::Application.config.secret_key_base = '1c1215dcc24f33888f2335c1b1276c17427a5502b7fbf45f9e436abf6a3409da94762af3703e1854ea166eb0a503348fa87fa8a2f3bf4f13f975a96decde498a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SenchaServer::Application.config.session_store :cookie_store, key: '_sencha_server_session'
