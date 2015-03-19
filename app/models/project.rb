0d7d2088dc2e51ec56ef52ef33d48528e687a306
yokoi.shinya <shinya.yokoi@gmail.com>
2015-03-19 12:42:18 +0900

    testing for model of project 1

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ActiveRecord::Base
   has_many :events, dependent: :destroy
+  validates :pjname, presence: true
+  validates :user_id, presence: true
   #self.primary_key = :user_id, :pjname
 
   accepts_nested_attributes_for :events
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @project = Project.new( pjname: "test",
+                            user_id: "1" )
+  end
+
+  subject { @project }
+
+  it { should respond_to(:pjname) }
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when pjname is not present" do
+    before { @project.pjname = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when user_id is not present" do
+    before { @project.user_id = " " }
+    it { should_not be_valid }
+  end
 end
