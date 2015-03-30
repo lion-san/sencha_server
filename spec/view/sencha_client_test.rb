d80e1fd6ca5164406be61ae0c3054e616e6729f1
yokoi.shinya <shinya.yokoi@gmail.com>
2015-03-30 10:19:56 +0900

    sagyo mae

---
+++ spec/view/sencha_client_test.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Sencha Client" do
+
+
+  subject { page }
+
+  before { visit '/' }
+
+  describe "Init Page" do
+
+
+    it { should have_content(page.body) }
+
+  end
+
+
+  describe "Create Project" do
+  end
+
+
+  describe "Load project List" do
+  end
+
+
+  describe "show Project List" do
+  end
+
+
+  describe "Load Project" do
+  end
+
+
+  describe "ReLoad Project" do
+  end
+
+
+  describe "Save Project" do
+  end
+
+
+end
